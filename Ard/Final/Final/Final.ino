595d3a4141d85c3c285654c11778f9c7ee8c058b
Mr-DareDevi <arnabsen1729@gmail.com>
2020-01-15 11:48:53 +0530

    Final

--- Ard/Final/Final/Final.ino
+++ Ard/Final/Final/Final.ino
@@ -50,37 +50,6 @@ void setup() {
   lcd.clear();
 }
 
-
-
-void loop() {
-
-  scanIrSensor();
-    Serial.println(irValues);
-   
-  if((irValues[0]=='0' || irValues[7]=='0')&& (1==0)){
-    Serial.println("Cross-Section");   
-  }else{
-    int sumLeft=0, sumRight=0;
-    for(int count=0; count<8; count++){
-      if(count<=3) sumLeft+=irValues[count];
-      else sumRight+=irValues[count];
-    }
-    if(irValues=="11111111") stopMotion();
-    else if(sumLeft==sumRight) forward();
-    else if(sumLeft>sumRight) {Serial.println("Right"); soft_left(); }
-    else { Serial.println("Left"); soft_right(); }
-  }
-    
-
-}
-
-void scanIrSensor(){
-  for(byte count =0; count<8; count++){
-    bitWrite(irSensors, count, !digitalRead(irPins[count]));
-    irValues[count]=digitalRead(irPins[count])+'0';
-  }
-}
-
 void hard_left(){
       digitalWrite(motorLpin1, HIGH);
       digitalWrite(motorLpin2, LOW);
@@ -102,6 +71,13 @@ void forward(){
       digitalWrite(motorRpin2, LOW);
 }
 
+void backward(){
+      digitalWrite(motorLpin1, LOW);
+      digitalWrite(motorLpin2, HIGH);
+      digitalWrite(motorRpin1, LOW);
+      digitalWrite(motorRpin2, HIGH);
+}
+
 void hard_right(){
       digitalWrite(motorLpin1, LOW);
       digitalWrite(motorLpin2, HIGH);
@@ -121,4 +97,98 @@ void stopMotion(){
       digitalWrite(motorLpin2, LOW);
       digitalWrite(motorRpin1, LOW);
       digitalWrite(motorRpin2, LOW);
+}
+
+
+
+void full_left(){
+      forward();
+      delay(100);
+      hard_left();
+      delay(200);
+      stopMotion();
+      delay(200);
+}
+
+void full_right(){
+      forward();
+      delay(100);
+      hard_right();
+      delay(200);
+      stopMotion();
+      delay(200);
+}
+
+void full_forward(){
+      forward();
+      delay(50);
+      stopMotion();
+      delay(150);
+}
+
+void correct_left(){
+      backward();
+      delay(70);;
+      soft_left();
+      delay(50);
+      stopMotion();
+      delay(100);
+}
+
+void correct_right(){
+      backward();
+      delay(70);
+      soft_right();
+      delay(50);
+      stopMotion();
+      delay(100);
+}
+
+void scanIrSensor(){
+  for(byte count =0; count<8; count++){
+    bitWrite(irSensors, count, !digitalRead(irPins[count]));
+    irValues[count]=digitalRead(irPins[count])+'0';
+  }
+}
+
+void scanIrSensorBW(){
+  for(byte count =0; count<8; count++){
+    bitWrite(irSensors, count, !digitalRead(irPins[count]));
+    irValues[count]=!digitalRead(irPins[count])+'0';
+  }
+}
+
+
+
+void loop() {
+
+  scanIrSensorBW();
+    Serial.println(irValues);
+   
+  if((irValues[0]=='0' || irValues[7]=='0')&& (1==0)){
+    Serial.println("Cross-Section");   
+  }else{
+    int sumLeft=0, sumRight=0;
+    for(int count=0; count<8; count++){
+      if(count<=3) sumLeft+=irValues[count];
+      else sumRight+=irValues[count];
+    }
+    if(irValues=="11111111") stopMotion();
+    else if(irValues[7]=='0' && irValues[6]=='0' && irValues[5]=='0'){
+      full_left();
+    }else if(irValues[0]=='0' && irValues[1]=='0' && irValues[2]=='0'){
+      full_right();
+    }
+    else if(irValues[7]=='0' || irValues[6]=='0'){
+      correct_left();
+    }else if(irValues[0]=='0' || irValues[1]=='0'){
+      correct_right();
+    }
+    
+    else{
+      full_forward();
+    }
+  }
+    
+
 }
